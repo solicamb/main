2dbfc5ff2ef2c5dc72a96014af793839aa65f774
Doug-vN <douglascvn@gmail.com>
2019-08-01 23:15:34 +0100

    Functioning message passing routine with string support

--- object_oriented_peripheral_interphace/Master_Sketch/Master_Sketch.ino
+++ object_oriented_peripheral_interphace/Master_Sketch/Master_Sketch.ino
@@ -1,4 +1,76 @@
 #include <SPI.h>
+#include <Arduino.h>
+
+template <typename T> unsigned int SPI_writeAnything (const T& value)
+  {
+    const byte * p = (const byte*) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof value; i++)
+          transferAndWait(*p++);
+    return i;
+  }  // end of SPI_writeAnything
+
+template <typename T> unsigned int SPI_readAnything(T& value)
+  {
+    byte * p = (byte*) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof value; i++)
+          *p++ = transferAndWait (0);
+    return i;
+  }  // end of SPI_readAnything
+  
+  
+template <typename T> unsigned int SPI_readAnything_ISR(T& value)
+  {
+    byte * p = (byte*) &value;
+    unsigned int i;
+    *p++ = SPDR;  // get first byte
+    for (i = 1; i < sizeof value; i++)
+          *p++ = transferAndWait (0);
+    return i;
+  }  // end of SPI_readAnything_ISR  
+
+typedef enum sInstruct{
+  DisplayInstructionAndWait,
+  DisplayInstructionAndWaitForUser,
+  DontDisplayAndWait,
+  DontDisplayAndContinue,
+  ACK,
+  Yes,
+  No,
+  NAK,
+  ReferToInt,
+  ReferToFloat,
+  ReferToString
+};
+
+typedef enum mInstruct{
+  PauseMeasurementForiParam,
+  RestartMeasurementProcedure,
+  ResetDevice,
+  HowManyInstructions,
+  NextCommandPlease,
+  IsThereData,
+  SendDataPlease,
+  WhoAreYou,
+  HowLongShouldIWait,
+  BeginMeasurement
+};
+
+typedef struct sCmd{
+  sInstruct Instruction; 
+  char sParam[128];
+  int iParam;
+  float fParam;
+};
+
+typedef struct mCmd{
+  mInstruct Instruction;
+  int iParam;
+  float fParam;
+};
+
+//############################################################################################################
 
 void setup (void)
 {
@@ -34,12 +106,26 @@ void loop (void)
   transferAndWait ('?');  // add command
   a = transferAndWait (0x00);
 
+  ///////////////////////////////////////////////////////
+  mCmd Request = {(mInstruct)IsThereData, (int)10,(float)1.5 };
+  SPI_writeAnything(Request);
+
+  delay(100);
+  sCmd Reply;
+  SPI_readAnything(Reply);
+ 
+  ///////////////////////////////////////////////////////
+
 
   // disable Slave Select
   digitalWrite(SS, HIGH);
 
   Serial.println ("Reply From Slave");
   Serial.println (a);
+  Serial.println(Reply.Instruction);
+  Serial.println((String)Reply.sParam);  
+  Serial.println(Reply.iParam);
+  Serial.println(Reply.fParam);
   
   delay (1000);  // 1 second delay 
 }  // end of loop
--- object_oriented_peripheral_interphace/Slave_Sketch/Slave_Sketch.ino
+++ object_oriented_peripheral_interphace/Slave_Sketch/Slave_Sketch.ino
@@ -1,6 +1,77 @@
 #include <SPI.h>
+#include <Arduino.h>
 
-volatile byte command = 0;
+template <typename T> unsigned int SPI_writeAnything (const T& value)
+  {
+    const byte * p = (const byte*) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof value; i++)
+          SPI.transfer(*p++);
+    return i;
+  }  // end of SPI_writeAnything
+
+template <typename T> unsigned int SPI_readAnything(T& value)
+  {
+    byte * p = (byte*) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof value; i++)
+          *p++ = SPI.transfer (0);
+    return i;
+  }  // end of SPI_readAnything
+  
+  
+template <typename T> unsigned int SPI_readAnything_ISR(T& value)
+  {
+    byte * p = (byte*) &value;
+    unsigned int i;
+    *p++ = SPDR;  // get first byte
+    for (i = 1; i < sizeof value; i++)
+          *p++ = SPI.transfer (0);
+    return i;
+  }  // end of SPI_readAnything_ISR  
+
+typedef enum sInstruct{
+  DisplayInstructionAndWait,
+  DisplayInstructionAndWaitForUser,
+  DontDisplayAndWait,
+  DontDisplayAndContinue,
+  ACK,
+  Yes,
+  No,
+  NAK,
+  ReferToInt,
+  ReferToFloat,
+  ReferToString
+};
+
+typedef enum mInstruct{
+  PauseMeasurementForiParam,
+  RestartMeasurementProcedure,
+  ResetDevice,
+  HowManyInstructions,
+  NextCommandPlease,
+  IsThereData,
+  SendDataPlease,
+  WhoAreYou,
+  HowLongShouldIWait,
+  BeginMeasurement
+};
+
+typedef struct sCmd{
+  sInstruct Instruction; 
+  char sParam[128];
+  int iParam;
+  float fParam;
+};
+
+typedef struct mCmd{
+  mInstruct Instruction;
+  int iParam;
+  float fParam;
+};
+
+
+
 
 void setup (void)
 {
@@ -24,6 +95,17 @@ ISR (SPI_STC_vect)
 
   if (c == '?'){
     SPI.transfer(0x06);
+  }
+
+
+/////////////////////////////////////////////////////////////////
+
+  mCmd Request;
+  SPI_readAnything(Request);
+
+  if (Request.Instruction == IsThereData){
+     sCmd Reply = {Yes,"This is a test message",Request.iParam +1, Request.fParam*2.0};
+     SPI_writeAnything(Reply);
   }
 
 
